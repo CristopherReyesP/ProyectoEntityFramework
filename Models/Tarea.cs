574039e3290b47bbf4b8a7688185292e71ca0a1d
CristopherReyesP <cristoreyepj_11@hotmail.com>
2023-08-05 02:29:07 -0600

    endpoints agregados

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace proyectoef.Models;
 
@@ -17,6 +18,7 @@ public class Tarea
     public string Descripcion { get; set; }
     public Prioridad PrioridadTarea { get; set; }
     public DateTime FechaCreacion { get; set; }
+    
     public virtual Categoria Categoria { get; set; }
    // [NotMapped]
     public string Resumen { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoef;
+using proyectoef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +12,55 @@ builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnection
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => {
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria:"+ dbContext.Database.IsInMemory());
+    return Results.Ok("Base de datos en memoria:" + dbContext.Database.IsInMemory());
+});
+//obtener datos
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
+{
+    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));
 });
 
+//Guardar datos
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok("Agregado");
+});
+
+//Actualizar datos
+app.MapPost("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+    if (tareaActual != null){
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+        
+        await dbContext.SaveChangesAsync();
+    
+        return Results.Ok("");
+    }
+
+        return Results.NotFound();
+});
+
+//Eliminar datos
+app.MapPost("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+    if(tareaActual != null){
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+    return Results.NotFound();
+});
 
 app.Run();
